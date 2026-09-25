304b560075019b90d5ca0ecdf492a476c12b84e8
Prashant Rao <prashant.rao-ext+hpeprod@hpe.com>
2026-09-25 22:49:05 +0530

    compile

--- Hello.java
+++ Hello.java
@@ -1,5 +1,13 @@
 public class Hello{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + (a * b));
+        System.out.println("Difference: " + (a - b));
+        System.out.println("Quotient: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+        System.out.println("All operations completed.");
     }
 }
